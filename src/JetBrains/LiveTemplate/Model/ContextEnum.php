134ba28343fd834c8bb0d01b4fa25fe60ce03dd8
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2016-02-27 14:58:22 +0100

    Add info about ace type to the ContextEnum

--- src/JetBrains/LiveTemplate/Model/ContextEnum.php
+++ src/JetBrains/LiveTemplate/Model/ContextEnum.php
@@ -12,28 +12,75 @@ namespace kaluzki\JetBrains\LiveTemplate\Model;
  */
 interface ContextEnum
 {
+    /**
+     * html
+     */
     const HTML            = "HTML";
     const HTML_TEXT       = "HTML_TEXT";
+
+    /**
+     * xml
+     */
     const XML             = "XML";
     const XML_TEXT        = "XML_TEXT";
     const XSL_TEXT        = "XSL_TEXT";
+
+    /**
+     * json
+     */
     const JSON            = "JSON";
+
+    /**
+     * sql
+     */
     const SQL             = "SQL";
+
+    /**
+     * jsp
+     */
     const JSP             = "JSP";
+
+    /**
+     * css
+     */
     const CSS             = "CSS";
-    const CSS_VALUE       = "CSS_PROPERTY_VALUE";
     const CSS_DECLARATION = "CSS_DECLARATION_BLOCK";
+    const CSS_VALUE       = "CSS_PROPERTY_VALUE";
     const CSS_RULESET     = "CSS_RULESET_LIST";
-    const CUCUMBER        = "CUCUMBER_FEATURE_FILE";
+
+    /**
+     * javascript
+     */
     const JS              = "JAVA_SCRIPT";
     const JS_EXPRESSION   = "JS_EXPRESSION";
     const JS_STATEMENT    = "JS_STATEMENT";
+    const JS_XHTML        = "JSX_HTML";
+
+    /**
+     * typescript
+     */
     const TYPESCRIPT      = "TypeScript";
-    const HAML            = "HAML";
+
+    /**
+     * coffee
+     */
+    const COFFEESCRIPT    = "CoffeeScript";
+
+    /**
+     * php
+     */
     const PHP             = "PHP";
     const PHP_COMMENT     = "PHP Comment";
     const PHP_STRING      = "PHP String Literal";
-    const JADE            = "JADE";
-    const COFFEESCRIPT    = "CoffeeScript";
+
+    /**
+     * haml
+     */
+    const HAML            = "HAML";
+
+    /**
+     * text
+     */
+    const CUCUMBER        = "CUCUMBER_FEATURE_FILE";
     const OTHER           = "OTHER";
 }
